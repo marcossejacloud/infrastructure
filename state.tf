7f3d600f2fa5add02c4f288ebc00306ba256cfe7
thiagolsfortunato <thiagolsfortunato@hotmail.com>
2020-03-24 19:24:23 -0300

    fix workflow to deploy staging project

--- state.tf
+++ state.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "covidzero-tfstate"
-    key = "staging/covidzero.tfstate"
+    key    = "staging/covidzero.tfstate"
     region = "us-east-2"
   }
 }
