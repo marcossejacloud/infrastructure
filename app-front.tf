954254b7674eb6d05398d4d21323d2fceb1548e4
Cassio Moreto <cassio.moreto@contaazul.com>
2020-03-28 00:53:54 -0300

    Created S3 Static website module

--- app-front.tf
+++ app-front.tf
@@ -1,12 +1,50 @@
 module "app_front" {
-  source             = "./modules/s3"
-  name               = "app-front-${var.app_name}"
+  source             = "./modules/s3/static-website"
+  name               = "app-staging"
+  base_domain        = var.base_domain
   versioning_enabled = true
+  sourceIps = [
+    "2400:cb00::/32",
+    "2405:8100::/32",
+    "2405:b500::/32",
+    "2606:4700::/32",
+    "2803:f800::/32",
+    "2c0f:f248::/32",
+    "2a06:98c0::/29",
+    "103.21.244.0/22",
+    "103.22.200.0/22",
+    "103.31.4.0/22",
+    "104.16.0.0/12",
+    "108.162.192.0/18",
+    "131.0.72.0/22",
+    "141.101.64.0/18",
+    "162.158.0.0/15",
+    "172.64.0.0/13",
+    "173.245.48.0/20",
+    "188.114.96.0/20",
+    "190.93.240.0/20",
+    "197.234.240.0/22",
+    "198.41.128.0/17",
+    "173.245.48.0/20",
+    "103.21.244.0/22",
+    "103.22.200.0/22",
+    "103.31.4.0/22",
+    "141.101.64.0/18",
+    "108.162.192.0/18",
+    "190.93.240.0/20",
+    "188.114.96.0/20",
+    "197.234.240.0/22",
+    "198.41.128.0/17",
+    "162.158.0.0/15",
+    "104.16.0.0/12",
+    "172.64.0.0/13",
+    "131.0.72.0/22"
+  ]
 }
 
 module "hostname_app_front" {
   source      = "./modules/ns"
-  app_name    = var.hostname_app_front_default != "" ? var.hostname_app_front_default : var.app_name
+  app_name    = "app-staging"
   alb_url     = module.app_front.bucket_domain_name
   base_domain = var.base_domain
   is_proxied  = true
